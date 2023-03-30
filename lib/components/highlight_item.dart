661e0c3621519bd6eafbb734e8f4061bb413d0c2
Thiago Ferreira <thiaago.dev@gmail.com>
2023-03-30 14:10:57 -0300

    estilizando texto e imagem dos highlights

--- lib/components/highlight_item.dart
+++ lib/components/highlight_item.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:panucci_ristorante/themes/app_colors.dart';
 
 class HighlightItem extends StatelessWidget {
-const HighlightItem({ Key? key, required this.imageURI, required this.itemTitle, required this.itemPrice, required this.itemDescription}) : super(key: key);
-final String imageURI;
-final String itemTitle;
-final String itemPrice;
-final String itemDescription;
+  const HighlightItem(
+      {Key? key,
+      required this.imageURI,
+      required this.itemTitle,
+      required this.itemPrice,
+      required this.itemDescription})
+      : super(key: key);
+  final String imageURI;
+  final String itemTitle;
+  final String itemPrice;
+  final String itemDescription;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.hardEdge,
       color: Theme.of(context).colorScheme.surfaceVariant,
@@ -19,13 +25,18 @@ final String itemDescription;
           Image(
             image: AssetImage(imageURI),
             fit: BoxFit.cover,
+            height: 120,
+            width: double.infinity,
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(itemTitle, style: const TextStyle(fontSize: 16),),
+                Text(
+                  itemTitle,
+                  style: const TextStyle(fontSize: 16),
+                ),
                 Text("R\$ " + itemPrice),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -10,10 +10,19 @@ class Highlights extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
       child: CustomScrollView(
         slivers: <Widget>[
-          const SliverToBoxAdapter(child: Text('Destaques')),
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Destaques',
+                style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (context, index) {
