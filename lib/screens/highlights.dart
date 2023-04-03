c9a3b7a3719fd6c0e6c578c926ce33b410aa2ce5
Thiago Ferreira <thiaago.dev@gmail.com>
2023-04-03 17:11:13 -0300

    adicionando layout responsivo na pagina de destaques

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -5,8 +5,6 @@ import 'package:panucci_ristorante/components/highlight_item.dart';
 class Highlights extends StatelessWidget {
   const Highlights({super.key});
 
-  final List items = destaques;
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,20 +21,60 @@ class Highlights extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return HighlightItem(
-                  imageURI: items[index]["image"],
-                  itemTitle: items[index]["name"],
-                  itemPrice: items[index]["price"],
-                  itemDescription: items[index]["description"],
-                );
-              },
-              childCount: items.length,
-            ),
-          ),
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? _landScapeList()
+              : _portraitList(),
         ],
+      ),
+    );
+  }
+}
+
+class _portraitList extends StatelessWidget {
+  final List items = destaques;
+  const _portraitList();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+    );
+  }
+}
+
+class _landScapeList extends StatelessWidget {
+  final List items = destaques;
+  const _landScapeList();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        ((context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        }),
+        childCount: items.length,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 1,
       ),
     );
   }
